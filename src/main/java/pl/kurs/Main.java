78207d319e8d0ee741ba57ca62c879aeeed7146b
Magdalena Pikulska <m.e.pikulska@gmail.com>
2023-07-21 19:25:27 +0200

    Added logic of saving history event

--- src/main/java/pl/kurs/Main.java
+++ src/main/java/pl/kurs/Main.java
@@ -2,6 +2,9 @@ package pl.kurs;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Container c1 = Container.create("pierwszy", 10, 5);
+        Container c2 = Container.create("drugi", 10, 5);
+
+        c1.transferWater(c2, 100);
     }
 }
